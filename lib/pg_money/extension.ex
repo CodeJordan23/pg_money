ef84680fcaba6e737fe91966938cdf04e64cd03e
Michael J <codejordan23+cs@gmail.com>
2019-11-15 22:21:41 +0100

    updated doc and refactored a bit

--- lib/pg_money/extension.ex
+++ lib/pg_money/extension.ex
@@ -1,6 +1,6 @@
 defmodule PgMoney.Extension do
   @moduledoc """
-  Implements how to encode and decode PostgeSQL's `money` data type.
+  Implements how to encode and decode PostgeSQL's [`money` data type](https://www.postgresql.org/docs/9.5/datatype-money.html).
   """
 
   @behaviour Postgrex.Extension
@@ -10,8 +10,19 @@ defmodule PgMoney.Extension do
   @max_int_val +9_223_372_036_854_775_807
   @storage_size 8
 
+  @doc """
+  Defines the minimum integer value possible for the `money` type.
+  """
   def min_int_val, do: @min_int_val
+
+  @doc """
+  Defines the maximum integer value possible for the `money` type.
+  """
   def max_int_val, do: @max_int_val
+
+  @doc """
+  Defines the size in the database.
+  """
   def storage_size, do: @storage_size
 
   @impl true
@@ -33,42 +44,46 @@ defmodule PgMoney.Extension do
   def decode(_state) do
     quote location: :keep do
       <<unquote(@storage_size)::int32, data::binary-size(unquote(@storage_size))>> ->
-        unquote(__MODULE__).decode_numeric(data)
+        unquote(__MODULE__).to_decimal(data)
     end
   end
 
-  def decode_numeric(<<digits::int64>>) do
+  def to_decimal(<<digits::int64>>) do
     Decimal.div(Decimal.new(digits), Decimal.new(100))
   end
+  def to_decimal(other) do
+    raise ArgumentError, "cannot represent #{inspect(other)} as money, not a valid int64."
+  end
+
 
   @impl true
   def encode(_state) do
     quote location: :keep do
-      %Decimal{} = decimal -> unquote(__MODULE__).encode_numeric(decimal)
-      n when is_float(n) -> unquote(__MODULE__).encode_numeric(Decimal.from_float(n))
-      n when is_integer(n) -> unquote(__MODULE__).encode_numeric(Decimal.new(n))
+      %Decimal{} = decimal -> unquote(__MODULE__).to_binary(decimal)
+      n when is_float(n) -> unquote(__MODULE__).to_binary(Decimal.from_float(n))
+      n when is_integer(n) -> unquote(__MODULE__).to_binary(Decimal.new(n))
+      other -> raise ArgumentError, "cannot encode #{inspect(other)} as money."
     end
   end
 
-  ## Helpers
-
-  def encode_numeric(%Decimal{coef: coef} = decimal) when coef in [:inf, :qNaN, :sNaN] do
-    raise ArgumentError, "cannot represent #{inspect(decimal)} as money type"
+  def to_binary(%Decimal{coef: coef} = decimal) when coef in [:inf, :qNaN, :sNaN] do
+    raise ArgumentError, "cannot represent #{inspect(decimal)} as money type."
+  end
+  def to_binary(%Decimal{sign: sign, coef: coef, exp: e} = d) do
+    case e do
+      0 -> encode_int(sign * coef * 100)
+      -1 -> encode_int(sign * coef * 10)
+      -2 -> encode_int(sign * coef)
+      _ -> to_binary(Decimal.round(d, 2))
+    end
   end
 
-  def encode_numeric(%Decimal{sign: sign, coef: coef, exp: 0}), do: encode_int(sign * coef * 100)
-  def encode_numeric(%Decimal{sign: sign, coef: coef, exp: -1}), do: encode_int(sign * coef * 10)
-  def encode_numeric(%Decimal{sign: sign, coef: coef, exp: -2}), do: encode_int(sign * coef)
-  def encode_numeric(%Decimal{} = dec), do: encode_numeric(Decimal.round(dec, 2))
-
-  defp encode_int(int) when is_integer(int) and int < @min_int_val do
+  defp encode_int(int) when int < @min_int_val do
     raise ArgumentError, "#{inspect(int)} exceeds money's min value #{inspect(@min_int_val)}"
   end
-
-  defp encode_int(int) when is_integer(int) and @max_int_val < int do
+  defp encode_int(int) when @max_int_val < int do
     raise ArgumentError, "#{inspect(int)} exceeds money's max value #{inspect(@max_int_val)}"
   end
-
   defp encode_int(int) when is_integer(int) do
     <<@storage_size::int32, int::int64>>
   end
