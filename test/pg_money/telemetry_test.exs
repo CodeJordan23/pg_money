ee918517bbee7938b0f060358ba42f40913bf1f0
Michael J <codejordan23+cs@gmail.com>
2019-11-17 13:24:07 +0100

    added some telemetry properties

---
+++ test/pg_money/telemetry_test.exs
@@ -0,0 +1,112 @@
+defmodule PgMoney.TelemetryTest do
+  use ExUnit.Case, async: false
+  use PropCheck
+
+  setup do
+    {:ok, conn} = start_supervised({Postgrex, PgMoney.Test.Helper.db_opts()})
+    %{conn: conn, precision: 2}
+  end
+
+  test "events are emitted", _ do
+    {id, ns} = create_redir(:pg_money_test, self())
+
+    try do
+      d = Decimal.new(0)
+      PgMoney.Extension.to_int(d, 2, ns)
+      {:msg, _} = receive_one()
+      assert true
+    after
+      :telemetry.detach(id)
+    end
+  end
+
+  property "rounding diff adds up to original", [], _ do
+    forall {p, {_i, decimal}} <- PgMoney.Prop.Helper.decimal_any_precision() do
+      {id, ns} = create_redir(:pg_money_test, self())
+
+      try do
+        src_diff =
+          "1e-#{p + 1}"
+          |> Decimal.new()
+
+        subject = Decimal.add(decimal, src_diff)
+        PgMoney.Extension.to_int(subject, p, ns)
+        {:msg, msg} = receive_one()
+
+        case msg.event do
+          [:pg_money_test, :lossy] ->
+            summed = Decimal.add(msg.data.dst, msg.data.diff)
+
+            Decimal.eq?(src_diff, msg.data.diff) &&
+              Decimal.eq?(subject, msg.meta.src) &&
+              Decimal.eq?(subject, summed)
+
+          _ ->
+            false
+        end
+        |> PropCheck.collect(PropCheck.with_title(:precision), p)
+      after
+        :telemetry.detach(id)
+      end
+    end
+  end
+
+  property "fake precision do not result in fake lossy messages", [] do
+    forall {p, {_i, decimal}} <- PgMoney.Prop.Helper.decimal_any_precision() do
+      {id, ns} = create_redir(:pg_money_test, self())
+
+      try do
+        factor = "1.#{String.duplicate("0", p + 1)}"
+        subject = Decimal.mult(factor, decimal)
+        PgMoney.Extension.to_int(subject, p, ns)
+        {:msg, msg} = receive_one()
+
+        case msg.event do
+          [:pg_money_test, :lossless] ->
+            true
+
+          _ ->
+            false
+        end
+        |> PropCheck.collect(with_title(:precision), p)
+      after
+        :telemetry.detach(id)
+      end
+    end
+  end
+
+  defp create_redir(ns, to) do
+    handler_id = UUID.uuid4()
+
+    events = [
+      [ns, :lossless],
+      [ns, :lossy]
+    ]
+
+    :ok =
+      :telemetry.attach_many(
+        handler_id,
+        events,
+        &handle_event/4,
+        to
+      )
+
+    {handler_id, [ns]}
+  end
+
+  defp handle_event(msg, data, meta, send_to) do
+    send(send_to, %{
+      event: msg,
+      data: data,
+      meta: meta
+    })
+  end
+
+  defp receive_one(timeout \\ 10) do
+    receive do
+      msg -> {:msg, msg}
+    after
+      timeout -> :no_msg
+    end
+  end
+end
